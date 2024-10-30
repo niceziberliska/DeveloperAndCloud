b6b0455cc370ce4f7466697fb2b6a3f7eb267b11
niceziberliska <max.tornqvist3@outlook.com>
2024-10-30 19:27:56 +0100

    winget search --name "sql", "work"
    winget install --name "MySQL Workbench 8.0 CE"
    
    winget install --id Oracle.VirtualBox -e --source
    winget

--- GetAndInstallSQLWorkbench8.0CE.ps1
+++
@@ -1,3 +0,0 @@
-﻿winget search --name "sql", "work" 
-
-winget install --name "MySQL Workbench 8.0 CE"
---
+++ Winget.ps1
@@ -0,0 +1,10 @@
+﻿
+
+###Get and install MySQL Workbench 8.0 CE
+
+winget search --name "sql", "work" 
+
+winget install --name "MySQL Workbench 8.0 CE"
+
+###Get And install Oracle VirtualBox
+winget install --id Oracle.VirtualBox -e --source winget
