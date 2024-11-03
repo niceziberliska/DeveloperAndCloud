2b210dec72f12e17525e6ebae53277e0125570e9
niceziberliska <max.tornqvist3@outlook.com>
2024-11-03 12:34:59 +0100

    Add an simple web application sample app for traning purpose

--- Winget.ps1
+++ Winget.ps1
@@ -1,10 +1,20 @@
 ﻿
+$input = Read-Host "Enter 1 for uppdating all software compatibe with winget"
 
+switch ($input) {
+    "1" { 
+        winget upgrade 
+        }
+    Default {}
+}
 ###Get and install MySQL Workbench 8.0 CE
 
+#get MySQL Workbench
 winget search --name "sql", "work" 
-
+#Installs MySQL Workbench 
 winget install --name "MySQL Workbench 8.0 CE"
 
 ###Get And install Oracle VirtualBox
-winget install --id Oracle.VirtualBox -e --source winget
+winget install --id Oracle.VirtualBox -e --source winget 
+
+#Check all software for update compatible with winget
